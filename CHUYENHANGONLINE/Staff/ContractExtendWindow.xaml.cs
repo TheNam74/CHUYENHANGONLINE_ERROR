1410f8d8c1fcbec43ac7323c0261911749be5b0e
Tung Nguyen <ncnt257@gmail.com>
2021-12-22 16:30:29 +0700

    update proc name

--- CHUYENHANGONLINE/Staff/ContractExtendWindow.xaml.cs
+++ CHUYENHANGONLINE/Staff/ContractExtendWindow.xaml.cs
@@ -47,7 +47,7 @@ namespace CHUYENHANGONLINE.Staff
             {
                 _provider.ContractDate = DatePicker.SelectedDate;
                 //using (SqlCommand cmd = new SqlCommand("USP_GIAHANHOPDONG_DOITAC", MainWindow.sqlCon))
-                using (SqlCommand cmd = new SqlCommand("USP_CAU4_5a", MainWindow.sqlCon))//gây ra lỗi deadlock conversion Tung - err5
+                using (SqlCommand cmd = new SqlCommand("USP_CAU4_3a", MainWindow.sqlCon))//gây ra lỗi deadlock conversion Tung - err5
                 {
                     cmd.CommandType = CommandType.StoredProcedure;
                     cmd.Parameters.Add("@maDoiTac", SqlDbType.VarChar).Value = _provider.Id;
